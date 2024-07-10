daace1fbdbf21fad0b0deeffadffb61ff8243c2a
al-shaimon <alshaimon968@gmail.com>
2024-07-10 21:40:15 +0600

    product interface

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,21 @@
+import { Model } from 'mongoose';
+
+export type TInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+export type TProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  image: string;
+  inventory: TInventory;
+};
+
+export interface ProductModel extends Model<TProduct> {
+  // eslint-disable-next-line no-unused-vars
+  isProductExists(name: string): Promise<TProduct | null>;
+}
